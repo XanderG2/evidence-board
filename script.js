2decb371328cdee86fc5073f823ce96ad0527ad0
Xander <XanderG2@users.noreply.github.com>
2026-01-17 17:55:30 +0000

    inital code n stuff

---
+++ script.js
@@ -0,0 +1,17 @@
+/**
+ * Save current evidence to computer
+ * TODO: create this function
+ */
+function save() {}
+
+/**
+ * Load current evidence from computer
+ * TODO: create this function
+ */
+function load() {}
+
+/**
+ * Add a node
+ * TODO: create this function
+ */
+function add() {}
